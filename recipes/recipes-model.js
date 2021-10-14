049b5b4607c1ad409c7d11f78244e8312a6ad02c
RodolfoJaspe <macosi@Rodolfos-iMac.local>
2021-10-13 23:40:00 -0400

    wip on findAllRecipes and findRecipeById recipes-model functions

---
+++ recipes/recipes-model.js
@@ -0,0 +1,18 @@
+const db = require("../data/db-config")
+
+function findAllRecipes(){
+    return db("recipes as r")
+}
+
+function findRecipeById(recipe_id){
+    return db("recipes as r")
+        .leftJoin("steps as s","r.recipe_id","s.recipe_id")
+        .leftJoin("step-ingredients as si","s.step_id","si.step_id")
+        .leftJoin("ingredients as i","si.ingredient_id","i.ingredient_id")
+        .leftJoin("categories as c","c.category_id","r.category_id")
+        .where("r.recipe_id",recipe_id)
+        .select("s.*","i.ingredient_name","s.instructions","c.category_name")
+
+}
+
+module.exports = {findAllRecipes,findRecipeById}

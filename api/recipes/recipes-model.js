da05e47db5201739ad9abd450ec0c10ea7d747d2
RodolfoJaspe <macosi@Rodolfos-iMac.local>
2021-10-18 20:30:15 -0400

    wip

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -3,22 +3,22 @@ const db = require("../../data/db-config")
 
 function findBy(user_id){
     return db("recipes as r")
-        .leftJoin("users as u","u.user_id","r.user_id")
-        .leftJoin("categories as c","r.category","c.category_id")
+        // .leftJoin("users as u","u.user_id","r.user_id")
+        // .leftJoin("categories as c","r.category_id","c.category_id")
         .where("r.user_id",user_id)
-        .select("u.user_id","u.username","r.title","r.source","c.category")
+        // .select("u.user_id","u.username","r.title","r.source","c.category")
 }
 
 function findById(recipe_id){
     return db("recipes as r")
-        .leftJoin("users as u","u.user_id","r.user_id")
-        .leftJoin("instructions as inst","r.recipe_id","inst.recipe_id")
-        .leftJoin("instruction-ingredients_table as ii","inst.instruction_id","ii.instruction_id")
-        .leftJoin("ingredients as ingr","ii.ingredient_id","ingr.ingredient_id")
-        .leftJoin("categories as c","c.category_id","r.category")
+        // .leftJoin("users as u","u.user_id","r.user_id")
+        // .leftJoin("instructions as inst","r.recipe_id","inst.recipe_id")
+        // .leftJoin("instruction_ingredient as ii","inst.instruction_id","ii.instruction_id")
+        // .leftJoin("ingredients as ingr","ii.ingredient_id","ingr.ingredient_id")
+        // .leftJoin("categories as c","c.category_id","r.category_id")
         .where("r.recipe_id",recipe_id)
-        .select("u.user_id","u.username","r.title*","r.source","ingr.ingredient_name","inst.instruction_content","inst.instruction_order","c.category")
-        .orderBy("inst.instruction_order")
+        // .select("u.user_id","u.username","r.title","r.source","ingr.ingredient_name","inst.instruction_content","inst.instruction_order","c.category")
+        // .orderBy("inst.instruction_order")
 }
 
 async function insert(recipe, user_id) {
--- api/users/users-middlware.js
+++ api/users/users-middlware.js
@@ -1,6 +1,24 @@
 const e = require("express")
 const User = require("./users-model")
+const jwt = require("jsonwebtoken")
+const {JWT_SECRET} = require("../secrets")
 
+const restricted = (req, res, next) => {
+    const token = req.headers.authorization
+
+    if(!token){
+        res.status(401).json({message:"Token required"})
+    }
+        jwt.verify(token,JWT_SECRET,(err,decoded) => {
+            if(err){
+                res.status(401).json({message:"Token invalid" + err.message})
+            }else {
+                req.decodedToken = decoded    
+            }
+            next()
+        })
+  
+}
 
 const ValidateUserNameUnique = async (req, res, next) => {
     const { username } = req.body
@@ -19,17 +37,17 @@ const ValidateUserNameUnique = async (req, res, next) => {
 
 const ValidateLogin = async (req, res, next) => {
   const { username } = req.body
+  const existingUser = await User.findBy({ username })
   if(username === undefined || username.length < 3) {
       next({ status: 400, message: 'Username must exist and be more than 3 characters!'})
+  } else if(typeof username !== 'string'){
+          res.status(400).json({message: 'username must not be a number!'})
+  } else if(!existingUser){
+          res.status(404).json({message:err})
   } else {
-      if(typeof username !== 'string') {
-          next({ status: 400, message: 'username must not be a number!'})
-      } else {
-          const existingUser = await User.findBy({ username })
-          req.user = existingUser
-          next()
-      }
+    req.user = existingUser
+    next()
   }
-} 
+}
 
-module.exports = { ValidateLogin, ValidateUserNameUnique}
+module.exports = { restricted, ValidateLogin, ValidateUserNameUnique}

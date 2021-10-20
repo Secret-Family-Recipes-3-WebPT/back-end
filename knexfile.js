f81bf0d06a3573c16effccf2f6a6f50bb90d4ba3
Your Name <you@example.com>
2021-10-19 21:51:18 -0500

    connected heroku and updated knexfile, also added scripts to the package.json

--- knexfile.js
+++ knexfile.js
@@ -1,40 +1,45 @@
 // Update with your config settings.
+require('dotenv').config()
+const pg = require('pg')
+
+if (process.env.DATABASE_URL) {
+  pg.defaults.ssl = { rejectUnauthorized: false }
+}
+
 
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'pg',
     useNullAsDefault: true,
-    connection: {
-      filename: './data/recipe_user_table.db3'
-    },
+    connection: process.env.DEV_DATABASE_URL,
     migrations: {
         directory: './data/migrations'
     },
     seeds:{
         directory: './data/seeds'
     },
-    pool: {
-        afterCreate: (conn, done) => {
-          conn.run('PRAGMA foreign_keys = ON', done)
-        },
-    }
+    // pool: {
+    //     afterCreate: (conn, done) => {
+    //       conn.run('PRAGMA foreign_keys = ON', done)
+    //     },
+    // }
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: 'knex_migrations',
+      directory: './data/migrations',
+    },
+    seeds:{
+      directory: './data/seeds'
+    },
   }
 
 };
